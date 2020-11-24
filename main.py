be784120455099283d781516f95e500675669988
Anastasia <n_vladimirceva@mail.ru>
2020-11-24 18:18:11 +0500

    кружочки

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import sys
 from random import randint
 
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5 import uic
 from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton
+from UI import Ui_MainWindow
 
 
-class Example(QMainWindow):
+class Example(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.setupUi(self)
         self.initUI()
 
     def initUI(self):
@@ -28,11 +28,13 @@ class Example(QMainWindow):
         self.repaint()
 
     def draw_flag(self, qp):
-        cnt = randint(5, 100)
+        cnt = randint(5, 25)
         qp.setBrush(QColor(255, 242, 0))
         for i in range(cnt):
             x, y = randint(0, self.width()), randint(0, self.height())
-            d = randint(10, 55)
+            r, g, b = randint(0, 255), randint(0, 255), randint(0, 255)
+            qp.setBrush(QColor(r, g, b))
+            d = randint(10, 275)
             qp.drawEllipse(x, y, d, d)
 
 
